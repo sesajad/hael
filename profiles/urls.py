4d8661dfacfb1bbc01595c3f89ffe7e9236ade4c
Amir Mohamad Ehsandar <amir.ehsandar@divar.ir>
2019-11-21 15:59:07 +0330

    Garbage collection job!

---
+++ profiles/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from profiles import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('accounts/register/', views.register, name='register'),
+]
