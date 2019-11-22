b5d81ab7b7804f048be13c939bef19904916c5b0
Sajad <sska1377@gmail.com>
2019-11-23 02:21:31 +0330

    a simple login may be faster

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-
 from profiles import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/login/', views.login, name='login'),
+    path('accounts/logout/', views.logout, name='logout'),
     path('accounts/register/', views.register, name='register'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
+from django.contrib import auth
+from django.conf import settings
 
 from profiles.forms import CreateUserForm
 from profiles.helpers import submit_client
 from profiles.models import *
+
+def login(request):
+    # TODO get redir from POST/GET
+    redirect_to = request.POST.get('next', request.GET.get('next', settings.LOGIN_REDIRECT_URL))
+
+    if request.user.is_authenticated:
+        return redirect(redirect_to)
+
+    if request.method == 'POST':
+        req = request.POST
+        user = auth.authenticate(username=req['username'], password=req['password'])
+        if not user:
+            return render(request,'login.html',{'error_message': 'Wrong password'})
+        else:
+            auth.login(request, user)
+            return redirect(redirect_to)
+
+    return render(request,'login.html',{'next': redirect_to})
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth.logout(request)
+    return redirect('login')
 
 
 @login_required
